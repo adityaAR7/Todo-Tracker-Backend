8cfa2c0019554593f34e3c9e39cb25e99ab8e48c
aditya_AR7 <adityaraizada.1123@gmail.com>
2023-12-15 23:20:17 +0530

    version 3

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as passport from "passport"
+import * as passport from 'passport';
 import * as cookieParser from 'cookie-parser';
-import * as session from "express-session"
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:process.env.CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE",
-    credentials:true
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
   });
-  app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false,  
-    cookie:{//for storing cookie in the client side
-      maxAge:24 * 60 * 60 * 1000,
-      secure:false
-    }
-  }))
-  app.use(cookieParser("My favourite numvber is 369"))
+  app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        //for storing cookie in the client side
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: false,
+      },
+    }),
+  );
+  app.use(cookieParser('My favourite numvber is 369'));
   app.use(passport.initialize());
   app.use(passport.session());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
