c5775e924db5567e90e79021b7f3d5d57cd809de
aditya_AR7 <adityaraizada.1123@gmail.com>
2023-12-16 00:25:26 +0530

    V10

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,6 +16,7 @@ export class AuthController {
       res.redirect('https://todo-tracker-production.up.railway.app');
     }
 
+    @UseGuards(AuthenticatedGuard)
     @Get('login/success')
     loginSuccess(@Req() req: Request, @Res() res: Response) {
       return res
