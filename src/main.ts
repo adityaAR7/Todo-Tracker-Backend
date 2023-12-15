91c2ea643ba311e9983b9a3ac70af47cfa3227e5
aditya_AR7 <adityaraizada.1123@gmail.com>
2023-12-16 00:17:24 +0530

    Version 8

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://todo-tracker-production.up.railway.app',
+    methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });
   app.use(
