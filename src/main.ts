026cb24d18eedcf99ab2a71444bcb7cda83178c9
aditya_AR7 <adityaraizada.1123@gmail.com>
2023-12-16 01:03:46 +0530

    V17

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as passport from 'passport';
 import * as session from 'express-session';
-const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,26 +10,18 @@ async function bootstrap() {
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });
-  // app.use(
-  //   session({
-  //     secret: process.env.SECRET,
-  //     resave: false,
-  //     saveUninitialized: true,
-  //     cookie: {
-  //       //for storing cookie in the client side
-  //       maxAge: 24 * 60 * 60 * 1000,
-  //       secure: true,
-  //       httpOnly:false,
-  //       sameSite:'none'
-  //     },
-  //   }),
-  // );
   app.use(
-    cookieSession({
-      name: "session",
-      keys: ['This is my secret'],
-      maxAge: 24 * 60 * 60 * 1000,
-    })
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        //for storing cookie in the client side
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: true,
+        httpOnly:false
+      },
+    }),
   );
   app.use(passport.initialize());
   app.use(passport.session());
